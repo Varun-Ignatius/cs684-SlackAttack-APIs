78269381f14fb2b352f821879ce54eb47e4a751d
Varun-Ignatius <36558991+Varun-Ignatius@users.noreply.github.com>
2023-02-26 18:18:04 -0500

    Removed UI templates, and added SignIn and SignUp APIs giving static response

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask,jsonify
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+@app.route("/signIn", methods = ['GET'])
+def signIn():
+    return jsonify({"Code": 200,
+        "Message": "Signed In Successfully"}) 
+
+@app.route("/signUp", methods = ['POST'])
+def signUp():
+    return jsonify({"Code": 200,
+        "Message": "Signed Up Successfully"}) 
 
 
 if __name__ == "__main__":
