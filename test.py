d6feeb9cfacf042e4d516012c6327bf29ae8dfd3
Varun-Ignatius <36558991+Varun-Ignatius@users.noreply.github.com>
2023-04-04 19:54:39 -0400

    unitTest code.

---
+++ test.py
@@ -0,0 +1,92 @@
+from app import app
+import unittest
+import json
+
+class APITest(unittest.TestCase):
+
+    #check if GET News is a valid API
+    def test_News_validRoute(self):
+        tester = app.test_client(self)
+        response = tester.get("/News/Jiraya-Sensei")
+        self.assertEqual(response.status_code, 200)
+
+    #check if GET News API has JSON response
+    def test_News_validContenType(self):
+        tester = app.test_client(self)
+        response = tester.get("/News/Jiraya-Sensei")
+        self.assertEqual(response.content_type, "application/json")
+
+    #check if GET News API does not give empty response
+    def test_News_validData(self):
+        tester = app.test_client(self)
+        response = tester.get("/News/Jiraya-Sensei")
+        self.assertTrue(response.data)
+
+     #check if GET Categories is a valid API
+    def test_Categories_validRoute(self):
+        tester = app.test_client(self)
+        response = tester.get("/Categories/Jiraya-Sensei")
+        self.assertEqual(response.status_code, 200)
+
+    #check if GET Categories API has JSON response
+    def test_Categories_validContenType(self):
+        tester = app.test_client(self)
+        response = tester.get("/Categories/Jiraya-Sensei")
+        self.assertEqual(response.content_type, "application/json")
+
+    #check if GET Categories API does not give empty response
+    def test_Categories_validData(self):
+        tester = app.test_client(self)
+        response = tester.get("/Categories/Jiraya-Sensei")
+        self.assertTrue(response.data)
+
+     #check if PUT Categories is a valid API
+    def test_Categories_Update_validRoute(self):
+         tester = app.test_client(self)
+         headers = {'content-type': 'application/json'}
+         data = { 
+            "business": True,
+            "entertainment": False,
+            "general": False,
+            "health": True,
+            "science": True,
+            "sports": False,
+            "technology": True
+        }
+         response = tester.put("/Categories/Jiraya-Sensei", data= json.dumps(data), headers= headers)
+         self.assertEqual(response.status_code, 200)
+
+     #check if PUT Categories API has JSON response
+    def test_Categories_Update_validContenType(self):
+         tester = app.test_client(self)
+         headers = {'content-type': 'application/json'}
+         data = { 
+            "business": True,
+            "entertainment": False,
+            "general": False,
+            "health": True,
+            "science": True,
+            "sports": False,
+            "technology": True
+        }
+         response = tester.put("/Categories/Jiraya-Sensei", data= json.dumps(data), headers= headers)
+         self.assertEqual(response.content_type, "application/json")
+
+     #check if PUT Categories API does not give empty response
+    def test_Categories_Update_validData(self):
+         tester = app.test_client(self)
+         headers = {'content-type': 'application/json'}
+         data = { 
+            "business": True,
+            "entertainment": False,
+            "general": False,
+            "health": True,
+            "science": True,
+            "sports": False,
+            "technology": True
+        }
+         response = tester.put("/Categories/Jiraya-Sensei", data= json.dumps(data), headers= headers)
+         self.assertTrue(response.data)
+
+if __name__ == "__main__":
+    unittest.main()
